5c8394dfbf13a9c18e7b39c7a57cfac3b1a87e61
Dev0071 <lecturergacheru71@gmail.com>
2023-03-13 17:34:31 +0300

    added more structure to my side bar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,16 +5,41 @@ import { social, links } from './data';
 
 const Sidebar = () => {
   return (
-    <Sidebar>
-      {links.map((link) => {
-        const { id, url, text } = link;
-        return (
-          <li key={id}>
-            <a href={url}>{text}</a>
-          </li>
-        );
-      })}
-    </Sidebar>
+    <aside className={`sidebar show-sidebar`}>
+      <div className='sidebar-header'>
+        <img
+          src={logo}
+          alt='logo'
+          className='logo'
+        />
+        <button className='close-btn'>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className='links'>
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className='social-icons'>
+        {social.map((item) => {
+          const { icon, url, id } = item;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
   );
 };
 
