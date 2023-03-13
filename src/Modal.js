3f2dac4a902260cfc89b58eb7e2dafbc27eb941e
Dev0071 <lecturergacheru71@gmail.com>
2023-03-13 18:52:39 +0300

    added toggle functionality to my modal and sidebar using useContext

--- src/Modal.js
+++ src/Modal.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useGlobalContext } from './context';
 import { FaTimes } from 'react-icons/fa';
+
 const Modal = () => {
+  const { isModalOpen, closeModal } = useGlobalContext();
+
   return (
-    <div className={`modal-overlay show-modal`}>
+    <div
+      className={`${
+        isModalOpen ? 'show-modal modal-overlay' : 'modal-overlay'
+      }`}
+    >
       <div className='modal-container'>
         <h3>modal content</h3>
-        <button className='close-modal-btn'>
+        <button
+          className='close-modal-btn'
+          onClick={closeModal}
+        >
           <FaTimes />
         </button>
       </div>
